e7d567e7322df024ece400fc0909935fb3347aa2
busraknar <b.ertas91@gmail.com>
2024-09-28 00:18:32 +0300

    Header is edited as Homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,47 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Ana Sayfa</Link></li>
-          <li><Link to="/about">Hakkında</Link></li>
-          <li><Link to="/team">Takım</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header id="header" className="header d-flex align-items-center fixed-top">
+            <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+                <a href="index.html" className="logo d-flex align-items-center">
+                    {/* Eğer bir resim logosu kullanmak isterseniz, aşağıdaki satırı kullanabilirsiniz */}
+                    {/* <img src="assets/img/logo.png" alt="logo" /> */}
+                    <h1 className="sitename">Söz Test</h1>
+                </a>
+
+                <nav id="navmenu" className="navmenu">
+                    <ul>
+                        <li><Link to="/" className="active">Anasayfa</Link></li>
+                        <li><Link to="/about">Hakkımızda</Link></li>
+                        <li><Link to="/services">Hizmetler</Link></li>
+                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><Link to="/team">Hazırlayanlar</Link></li>
+                        <li><Link to="/blog">Blog</Link></li>
+                        <li className="dropdown">
+                            <a href="#"><span>Sonuçlar</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
+                            <ul>
+                                <li className="dropdown">
+                                    <a href="#"><span>Sınıflar</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
+                                    <ul>
+                                        <li><Link to="/university1">Üniversite 1.Sınıf</Link></li>
+                                        <li><Link to="/university2">Üniversite 2.Sınıf</Link></li>
+                                        <li><Link to="/university3">Üniversite 3.Sınıf</Link></li>
+                                        <li><Link to="/university4">Üniversite 4.Sınıf</Link></li>
+                                        <li><Link to="/university5">Üniversite 5.Sınıf</Link></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.html">İletişim</a></li>
+                    </ul>
+                    <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+                </nav>
+
+            </div>
+        </header>
+    );
 };
 
 export default Header;
