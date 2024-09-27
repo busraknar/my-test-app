18655967c780faee5b6add0e6bcb33fb9350326f
busraknar <b.ertas91@gmail.com>
2024-09-28 00:17:26 +0300

    Homepage is Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Team from './components/Team';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Header />
+      <Main/>
+      <Footer />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Header />} />
         <Route path="/about" element={<About />} />
         <Route path="/team" element={<Team />} />
+        <Route path="/main" element={<Main />} />
+
       </Routes>
     </Router>
   );
