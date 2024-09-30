8d93e25d680ab840ed48cfb7a8fe0b6c82318e2a
busraknar <b.ertas91@gmail.com>
2024-09-30 23:05:28 +0300

    About tsx created

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import './about.css';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>Hakkında</h1>
-      <p>Bu sayfa hakkında bilgileri içerir.</p>
-    </div>
+    <section id="about" className="about section light-background">
+      <div className="container-about">
+        <div className="row gy-8">
+          <div className="col-lg-4 position-relative align-self-start">
+            <img src="assets/img/about.jpg" className="img-fluid" alt="About Us" />
+            <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" className="glightbox pulsating-play-btn"></a>
+          </div>
+          <div className="col-lg-6 content">
+            <h3>Özgün Test!</h3>
+            <p className="fst-italic">
+              Herkesin mutlu olduğu, doğru elemeyi sağlayan test.
+            </p>
+            <ul>
+              <li><i className="bi bi-check2-all"></i> İstediğiniz puanlama sistemi uygulanabilir.</li>
+              <li><i className="bi bi-check2-all"></i> Kolayca test sonuçlarına ulaşabilirsiniz.</li>
+              <li><i className="bi bi-check2-all"></i> Arayüz tasarımı konforlu.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
