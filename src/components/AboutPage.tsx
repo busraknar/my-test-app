2cbd3287f9202b3add584dbb1dfb02d47c307f8e
busraknar <b.ertas91@gmail.com>
2024-09-30 23:07:14 +0300

    AboutPage created

---
+++ src/components/AboutPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import About from './About';
+
+
+const AboutPage: React.FC = () => {
+  return (
+    <main className="main">
+      {/* Page Title */}
+      <About />
+         
+    </main>
+  );
+};
+
+export default AboutPage;
