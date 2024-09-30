4a5f98717e3d03a3a736f69f017e14e14a7283ae
busraknar <b.ertas91@gmail.com>
2024-09-30 23:10:49 +0300

    Home is header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
+
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Ana Sayfa</h1>
-      <p>Hoş geldiniz! Burası anasayfa.</p>
+      <Header/>
+  
     </div>
   );
 };
