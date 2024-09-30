e3cc5c2b3b07ae948768b04f2a670875e694b7db
busraknar <b.ertas91@gmail.com>
2024-09-30 23:09:39 +0300

    Footer is edited

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,16 @@ const Footer: React.FC = () => {
         <div className="container">
           <div className="row justify-content-center text-center">
             <div className="col-lg-6">
-              <h4>Join Our Newsletter</h4>
-              <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
+              <h4>Yeniliklerimizden Haberdar Olun</h4>
+              <p>Ürünlerimizden ve gelişimlerden haberdar olmak için tıklayın!</p>
               <form action="forms/newsletter.php" method="post" className="php-email-form">
                 <div className="newsletter-form">
                   <input type="email" name="email" />
                   <input type="submit" value="Subscribe" />
                 </div>
-                <div className="loading">Loading</div>
+                <div className="loading">Yükleniyor</div>
                 <div className="error-message"></div>
-                <div className="sent-message">Your subscription request has been sent. Thank you!</div>
+                <div className="sent-message">Takip isteğiniz gönderildi. Teşekkürler!</div>
               </form>
             </div>
           </div>
@@ -27,39 +27,37 @@ const Footer: React.FC = () => {
         <div className="row gy-4">
           <div className="col-lg-4 col-md-6 footer-about">
             <a href="index.html" className="d-flex align-items-center">
-              <span className="sitename">Moderna</span>
+              <span className="sitename">İstanbul Ticaret Üniversitesi</span>
             </a>
             <div className="footer-contact pt-3">
               <p>A108 Adam Street</p>
-              <p>New York, NY 535022</p>
+              <p>İstanbul, NY 535022</p>
               <p className="mt-3"><strong>Phone:</strong> +1 5589 55488 55</p>
               <p><strong>Email:</strong> info@example.com</p>
             </div>
           </div>
 
           <div className="col-lg-2 col-md-3 footer-links">
-            <h4>Useful Links</h4>
+            <h4>En Çok Tıklananlar</h4>
             <ul>
-              <li><i className="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i className="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i className="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i className="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i className="bi bi-chevron-right"></i> <a href="#">Anasayfa</a></li>
+              <li><i className="bi bi-chevron-right"></i> <a href="#">Hakkımızda</a></li>
+              <li><i className="bi bi-chevron-right"></i> <a href="#">Sonuçlar</a></li>
             </ul>
           </div>
 
           <div className="col-lg-2 col-md-3 footer-links">
-            <h4>Our Services</h4>
+            <h4>Servislerimiz</h4>
             <ul>
               <li><i className="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
               <li><i className="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i className="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
               <li><i className="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
             </ul>
           </div>
 
           <div className="col-lg-4 col-md-12">
-            <h4>Follow Us</h4>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+            <h4>Bizi Takip Et</h4>
+            <p>İstanbul Ticaret Üniversitesi Yazılım Akademisi</p>
             <div className="social-links d-flex">
               <a href="#"><i className="bi bi-twitter-x"></i></a>
               <a href="#"><i className="bi bi-facebook"></i></a>
