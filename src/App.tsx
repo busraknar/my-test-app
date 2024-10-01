e271baa399c7ef7ef0319d9695655424c836b965
busraknar <b.ertas91@gmail.com>
2024-10-01 21:24:22 +0300

    University Result added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
 import Team from './components/Team';
 import Footer from './components/Footer';
 import AboutPage from './components/AboutPage';
 import IndexPage from './components/IndexPage';
 import ReadingTest from './components/ReadingTest';
 import Contact from './components/Contact';
+import UniversityPage from './components/UniversityPage';  // Her sınıf için sonuçları gösterecek bileşendi ama
+import UniversityResults from './components/UniversityResults';
+
 
 const App: React.FC = () => {
   return (
     <Router>
-     <Home/>
+      <Home />
       <Routes>
+      
+        <Route path="/:className" element={<UniversityResults />} />
         <Route path="/" element={<IndexPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/team" element={<Team />} />
-        <Route path="/test" element={<ReadingTest/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/test" element={<ReadingTest />} />
+        <Route path="/contact" element={<Contact />} />
 
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 };
